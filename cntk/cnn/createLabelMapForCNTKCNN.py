de45c192996d863dafcae43c0816c5ca68bafff9
Guolin Ke <guoke@microsoft.com>
2016-09-12 13:28:41 +0800

    update cntk config

--- cntk/cnn/createLabelMapForCNTKCNN.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-dim = 1000
-a = range(0, dim)
-np.savetxt('./labelmap.txt', np.reshape(a, (dim, 1)), fmt='%d')
--- cntk/fcn/createLabelMapForCNTKFCN.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-dim = 26752
-a = range(0, dim)
-np.savetxt('./labelmap26752.txt', np.reshape(a, (dim, 1)), fmt='%d')
