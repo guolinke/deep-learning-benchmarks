1a3568a86bdacfd8c85c16d050239e0a72880f93
Guolin Ke <guoke@microsoft.com>
2016-09-12 20:06:13 +0800

    update result for caffe

---
+++ caffe/caffe_run_benchmarks.sh
@@ -0,0 +1,22 @@
+
+BASEDIR=$(dirname "$0")
+
+export PYTHONPATH=$PYTHONPATH:$BASEDIR/caffe/python
+
+sudo rm -f output_alexnet.log
+sudo rm -f output_resnet.log
+sudo rm -f output_fcn5.log
+sudo rm -f output_fcn8.log
+
+cd fcn
+python createFakeDataForCaffeFCN.py
+cd ..
+
+cd cnn
+python createFakeImageNetForCaffeCNN.py
+cd ..
+
+caffe/build/tools/caffe time -model=fcn/ffn26752-b64.prototxt -gpu=0 2>&1 | tee output_fcn5.log
+caffe/build/tools/caffe time -model=fcn/ffn26752l6-b64.prototxt -gpu=0 2>&1 | tee output_fcn8.log
+caffe/build/tools/caffe time -model=cnn/alexnet/alexnet-b16.prototxt -gpu=0 -iterations=100 2>&1 | tee output_alexnet.log
+caffe/build/tools/caffe time -model=cnn/resnet/resnet-b16.prototxt -gpu=0 -iterations=100 2>&1 | tee output_resnet.log
