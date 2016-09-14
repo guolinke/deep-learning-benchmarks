188943e8ddcee695b85c412433a385d1820e1800
Young <afe.young@gmail.com>
2016-09-14 12:47:38 +0800

    update alext model

--- theano/auto_benchmark.sh
+++ theano/auto_benchmark.sh
@@ -64,11 +64,11 @@ run_benchmark () {
 }
 
 run_benchmark alexnet $CMBS $CNB
-run_benchmark resnet $CMBS $CNB
-run_benchmark fcn5 $FMBS $FNB
-run_benchmark fcn8 $FMBS $FNB
-run_benchmark lstm32 $RMBS $RNB
-run_benchmark lstm64 $RMBS $RNB
+# run_benchmark resnet $CMBS $CNB
+# run_benchmark fcn5 $FMBS $FNB
+# run_benchmark fcn8 $FMBS $FNB
+# run_benchmark lstm32 $RMBS $RNB
+# run_benchmark lstm64 $RMBS $RNB
 
 
 # grep the result
