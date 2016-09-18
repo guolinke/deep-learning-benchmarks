bbfac50467b41d2c9b12805ab0d24dbd794f963a
Guolin Ke <guoke@microsoft.com>
2016-09-18 14:11:09 +0800

    Add script to run all benchmarks.

---
+++ run_all.sh
@@ -0,0 +1,20 @@
+cd caffe
+./caffe_auto.sh
+cd ..
+
+cd cntk
+./cntk_auto.sh
+cd ..
+
+cd tensorflow
+./tensorflow_auto.sh
+cd ..
+
+cd theano
+./theano_auto.sh
+cd ..
+
+cd torch
+./torch_auto.sh
+cd ..
+
