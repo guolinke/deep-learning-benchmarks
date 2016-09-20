465d04ea679fe0fa9d0a28b4d6f5c03c114ce64f
Guolin Ke <i@yumumu.me>
2016-09-20 17:40:34 +0800

    Update cntk_build.sh

--- cntk/cntk_build.sh
+++ cntk/cntk_build.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 sudo rm -rf cntk
 # remove old openmpi
-#sudo apt-get remove --auto-remove libopenmpi-dev
+sudo apt-get remove --auto-remove libopenmpi-dev
+
+mpi_home=/opt/openmpi-1.10.3
+wget https://www.open-mpi.org/software/ompi/v1.10/downloads/openmpi-1.10.3.tar.gz
+tar -xzvf ./openmpi-1.10.3.tar.gz
+cd openmpi-1.10.3
+./configure --prefix=${mpi_home}
+make -j all
+sudo make install
+cd ..
+
+export PATH=${mpi_home}/bin:$PATH
+export LD_LIBRARY_PATH=${mpi_home}/lib:$LD_LIBRARY_PATH
+
 sudo apt-get install libbz2-dev
 
 wget -q -O - https://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.gz/download | tar -xzf - 
