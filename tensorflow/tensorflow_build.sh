199d00e1fda4f86e7c665f60eb20617ea1ec4cd8
Guolin Ke <i@yumumu.me>
2016-09-20 17:12:51 +0800

    Update tensorflow_build.sh

--- tensorflow/tensorflow_build.sh
+++ tensorflow/tensorflow_build.sh
@@ -5,6 +5,7 @@ sudo apt-get install -y python-pip python-dev python-numpy swig python-dev pytho
 sudo rm -rf tensorflow
 git clone https://github.com/tensorflow/tensorflow 
 cd tensorflow
+git reset --hard bc64f05d4090262025a95438b42a54bfdc5bcc80
 sudo ./configure
 bazel build -c opt --config=cuda //tensorflow/tools/pip_package:build_pip_package
 bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
