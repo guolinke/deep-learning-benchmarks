93adf397272c851b02b44069204d135734356655
Guolin Ke <i@yumumu.me>
2016-09-21 22:14:35 +0800

    Update cntk_build.sh

--- cntk/cntk_build.sh
+++ cntk/cntk_build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sudo rm -rf cntk
+sudo rm -rf CNTK-1-7-Linux-64bit-GPU-1bit-SGD.tar.gz
 wget https://cntk.ai/BinaryDrop/CNTK-1-7-Linux-64bit-GPU-1bit-SGD.tar.gz
 sudo tar -zxf CNTK-1-7-Linux-64bit-GPU-1bit-SGD.tar.gz
