c7c09bc25f68b18323805d6f0785039d0040689b
Guolin Ke <i@yumumu.me>
2016-09-21 19:37:50 +0800

    Update caffe_build.sh

--- caffe/caffe_build.sh
+++ caffe/caffe_build.sh
@@ -1,5 +1,6 @@
 sudo apt-get install -y libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler
 sudo apt-get install -y --no-install-recommends libboost-all-dev
+sudo apt-get install -y libatlas-base-dev
 sudo apt-get install -y libgflags-dev libgoogle-glog-dev liblmdb-dev
 sudo rm -rf caffe
 git clone https://github.com/BVLC/caffe
