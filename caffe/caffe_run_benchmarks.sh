9f3cb61ba821aac34885028820429ea3f01418d3
Guolin Ke <i@yumumu.me>
2016-09-21 13:23:31 +0800

    use "caffe train" for accurate timing

--- caffe/caffe_run_benchmarks.sh
+++ caffe/caffe_run_benchmarks.sh
@@ -16,7 +16,7 @@ cd cnn
 python createFakeImageNetForCaffeCNN.py
 cd ..
 
-caffe/build/tools/caffe time -model=fcn/ffn26752-b64.prototxt -gpu=0 2>&1 | tee output_fcn5.log
-caffe/build/tools/caffe time -model=fcn/ffn26752l6-b64.prototxt -gpu=0 2>&1 | tee output_fcn8.log
-caffe/build/tools/caffe time -model=cnn/alexnet/alexnet-b16.prototxt -gpu=0 -iterations=100 2>&1 | tee output_alexnet.log
-caffe/build/tools/caffe time -model=cnn/resnet/resnet-b16.prototxt -gpu=0 -iterations=100 2>&1 | tee output_resnet.log
+caffe/build/tools/caffe train -model=fcn/ffn26752-b64.prototxt -gpu=0 2>&1 | tee output_fcn5.log
+caffe/build/tools/caffe train -model=fcn/ffn26752l6-b64.prototxt -gpu=0 2>&1 | tee output_fcn8.log
+caffe/build/tools/caffe train -model=cnn/alexnet/alexnet-b16.prototxt -gpu=0 -iterations=100 2>&1 | tee output_alexnet.log
+caffe/build/tools/caffe train -model=cnn/resnet/resnet-b16.prototxt -gpu=0 -iterations=100 2>&1 | tee output_resnet.log
