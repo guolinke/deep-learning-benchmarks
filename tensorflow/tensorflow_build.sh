843ec76786fefbf1d34c34bbfc1e66aa6d0ceee5
Guolin Ke <i@yumumu.me>
2016-09-21 17:12:28 +0800

    Update tensorflow_build.sh

--- tensorflow/tensorflow_build.sh
+++ tensorflow/tensorflow_build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.10.0-cp27-none-linux_x86_64.whl
 sudo pip install --upgrade $TF_BINARY_URL
+sudo pip install scikit-image
