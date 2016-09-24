0bf2bd7551bf98b1a8caa45bdc1517e0b393d3ce
Guolin Ke <i@yumumu.me>
2016-09-24 10:00:27 +0800

    Update symbol_alexnet.py

--- mxnet/cnn/symbol_alexnet.py
+++ mxnet/cnn/symbol_alexnet.py
@@ -12,16 +12,16 @@ def get_alexnet_symbol(num_classes = 1000):
     relu1 = mx.symbol.Activation(data=conv1, act_type="relu")
     pool1 = mx.symbol.Pooling(
         data=relu1, pool_type="max", kernel=(3, 3), stride=(2,2))
-    lrn1 = mx.symbol.LRN(data=pool1, alpha=0.0001, beta=0.75, knorm=1, nsize=5)
+    #lrn1 = mx.symbol.LRN(data=pool1, alpha=0.0001, beta=0.75, knorm=1, nsize=5)
     # stage 2
     conv2 = mx.symbol.Convolution(
-        data=lrn1, kernel=(5, 5), pad=(2, 2), num_filter=256)
+        data=pool1, kernel=(5, 5), pad=(2, 2), num_filter=256)
     relu2 = mx.symbol.Activation(data=conv2, act_type="relu")
     pool2 = mx.symbol.Pooling(data=relu2, kernel=(3, 3), stride=(2, 2), pool_type="max")
-    lrn2 = mx.symbol.LRN(data=pool2, alpha=0.0001, beta=0.75, knorm=1, nsize=5)
+    #lrn2 = mx.symbol.LRN(data=pool2, alpha=0.0001, beta=0.75, knorm=1, nsize=5)
     # stage 3
     conv3 = mx.symbol.Convolution(
-        data=lrn2, kernel=(3, 3), pad=(1, 1), num_filter=384)
+        data=pool2, kernel=(3, 3), pad=(1, 1), num_filter=384)
     relu3 = mx.symbol.Activation(data=conv3, act_type="relu")
     conv4 = mx.symbol.Convolution(
         data=relu3, kernel=(3, 3), pad=(1, 1), num_filter=384)
@@ -34,11 +34,11 @@ def get_alexnet_symbol(num_classes = 1000):
     flatten = mx.symbol.Flatten(data=pool3)
     fc1 = mx.symbol.FullyConnected(data=flatten, num_hidden=4096)
     relu6 = mx.symbol.Activation(data=fc1, act_type="relu")
-    dropout1 = mx.symbol.Dropout(data=relu6, p=0.5)
+    #dropout1 = mx.symbol.Dropout(data=relu6, p=0.5)
     # stage 5
-    fc2 = mx.symbol.FullyConnected(data=dropout1, num_hidden=4096)
+    fc2 = mx.symbol.FullyConnected(data=relu6, num_hidden=4096)
     relu7 = mx.symbol.Activation(data=fc2, act_type="relu")
-    dropout2 = mx.symbol.Dropout(data=relu7, p=0.5)
+    #dropout2 = mx.symbol.Dropout(data=relu7, p=0.5)
     # stage 6
-    fc3 = mx.symbol.FullyConnected(data=dropout2, num_hidden=num_classes)
+    fc3 = mx.symbol.FullyConnected(data=relu7, num_hidden=num_classes)
     return fc3
