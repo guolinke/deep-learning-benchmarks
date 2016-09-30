2d0c298a63883d25e34003d26b067f0e278f396a
Guolin Ke <i@yumumu.me>
2016-09-30 19:21:02 +0800

    change torch version to stable one.

--- torch7/torch_build.sh
+++ torch7/torch_build.sh
@@ -2,6 +2,7 @@
 
 sudo rm -rf torch
 git clone https://github.com/torch/distro.git torch --recursive 
+git reset --hard a86fdb060509234288c8b2c5eae2d3d66b51901a
 cd torch; bash install-deps
 sudo /usr/bin/yes | ./install.sh 
 source ~/.bashrc
