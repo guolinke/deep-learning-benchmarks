84b12e0d774a5c82d75b340da701c9b3d1ba9ea3
Guolin Ke <i@yumumu.me>
2016-10-12 11:34:27 +0800

    Update torch_build.sh

--- torch7/torch_build.sh
+++ torch7/torch_build.sh
@@ -3,7 +3,8 @@
 sudo rm -rf torch
 git clone https://github.com/torch/distro.git torch --recursive 
 git reset --hard a86fdb060509234288c8b2c5eae2d3d66b51901a
-cd torch; bash install-deps
+cd torch
+sudo bash install-deps
 sudo /usr/bin/yes | ./install.sh 
 source ~/.bashrc
 
