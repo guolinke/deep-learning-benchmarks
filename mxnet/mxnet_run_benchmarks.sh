da0e1a6d3f1d3c201130d0f1557191c61c8b2e0b
Guolin Ke <i@yumumu.me>
2016-10-12 12:00:22 +0800

    Update mxnet_run_benchmarks.sh

--- mxnet/mxnet_run_benchmarks.sh
+++ mxnet/mxnet_run_benchmarks.sh
@@ -3,8 +3,8 @@
 CMBS=16
 CNB=100
 
-LMBS=128
-LNB=100
+RMBS=128
+RNB=100
 
 FMBS=64
 FNB=100
@@ -12,40 +12,40 @@ FNB=100
 for i in "$@"
 do
 case $i in
-	-cm=*|--cnn_mini_batch_size=*)
-	CMBS="${i#*=}"
-	shift	# past argument=value
-	;;
-	-cn=*|--cnn_num_batch=*)
-	CNB="${i#*=}"
-	shift	# past argument=value
-	;;
+    -cm=*|--cnn_mini_batch_size=*)
+    CMBS="${i#*=}"
+    shift # past argument=value
+    ;;
+    -cn=*|--cnn_num_batch=*)
+    CNB="${i#*=}"
+    shift # past argument=value
+    ;;
 
-	-lm=*|--lstm_mini_batch_size=*)
-	LMBS="${i#*=}"
-	shift	# past argument=value
-	;;
-	-ln=*|--lstm_num_batch=*)
-	LNB="${i#*=}"
-	shift	# past argument=value
-	;;
-	
-	-fm=*|--fcn_mini_batch_size=*)
-	FMBS="${i#*=}"
-	shift	# past argument=value
-	;;
-	-fn=*|--fcn_num_batch=*)
-	FNB="${i#*=}"
-	shift	# past argument=value
-	;;
+    -rm=*|--rnn_mini_batch_size=*)
+    RMBS="${i#*=}"
+    shift # past argument=value
+    ;;
+    -rn=*|--rnn_num_batch=*)
+    RNB="${i#*=}"
+    shift # past argument=value
+    ;;
 
-	--default)
-	DEFAULT=YES
-	shift	# past argument=value
-	;;
-	*)
-		# unknown option
-	;;
+    -fm=*|--fcn_mini_batch_size=*)
+    fMBS="${i#*=}"
+    shift # past argument=value
+    ;;
+    -fn=*|--fcn_num_batch=*)
+    FNB="${i#*=}"
+    shift # past argument=value
+    ;;
+
+    --default)
+    DEFAULT=YES
+    shift # past argument with no value
+    ;;
+    *)
+            # unknown option
+    ;;
 esac
 done
 
@@ -60,5 +60,5 @@ python benchmark.py --arch alexnet --batch-size ${CMBS} --num-batch ${CNB} 2>&1 
 python benchmark.py --arch resnet --batch-size ${CMBS} --num-batch ${CNB}  2>&1 | tee output_resnet.log
 python benchmark.py --arch fcn5 --batch-size ${FMBS} --num-batch ${FNB} 2>&1 | tee output_fcn5.log
 python benchmark.py --arch fcn8 --batch-size ${FMBS} --num-batch ${FNB} 2>&1 | tee output_fcn8.log
-python rnn/lstm_bucketing.py --batch-size ${LMBS} --num-batch ${LNB} --seq-len 32 --data-path "../cntk/rnn/PennTreebank/Data" 2>&1 | tee output_lstm32.log
-python rnn/lstm_bucketing.py --batch-size ${LMBS} --num-batch ${LNB} --seq-len 64 --data-path "../cntk/rnn/PennTreebank/Data" 2>&1 | tee output_lstm64.log	
+python rnn/lstm_bucketing.py --batch-size ${RMBS} --num-batch ${RNB} --seq-len 32 --data-path "../cntk/rnn/PennTreebank/Data" 2>&1 | tee output_lstm32.log
+python rnn/lstm_bucketing.py --batch-size ${RMBS} --num-batch ${RNB} --seq-len 64 --data-path "../cntk/rnn/PennTreebank/Data" 2>&1 | tee output_lstm64.log	
