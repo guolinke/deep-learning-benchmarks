fcc84c9a22b26c224ed68149d5aa57272f6edcf9
Guolin Ke <i@yumumu.me>
2016-10-14 00:24:11 +0800

    add benchmark only scripts.

---
+++ run_benchmarks.sh
@@ -0,0 +1,24 @@
+cd caffe
+./caffe_run_benchmarks.sh "$@"
+cd ..
+
+cd cntk
+./cntk_run_benchmarks.sh "$@"
+cd ..
+
+cd mxnet
+./mxnet_run_benchmarks.sh "$@"
+cd ..
+
+cd tensorflow
+./tensorflow_run_benchmarks.sh "$@"
+cd ..
+
+cd theano
+./auto_benchmarks.sh "$@"
+cd ..
+
+cd torch7
+./torch_run_benchmarks.sh "$@"
+cd ..
+
