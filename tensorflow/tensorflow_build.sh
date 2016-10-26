1ecdd476f0cbeea635d62bd5f731869bae879094
Guolin Ke <guoke@microsoft.com>
2016-10-26 13:46:14 +0800

    Update tensorflow to r0.11

--- tensorflow/tensorflow_build.sh
+++ tensorflow/tensorflow_build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.10.0-cp27-none-linux_x86_64.whl
+export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.11.0rc1-cp27-none-linux_x86_64.whl
 sudo pip install --upgrade $TF_BINARY_URL
 sudo pip install scikit-image
+sudo pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/protobuf-3.0.0b2.post2-cp27-none-linux_x86_64.whl
