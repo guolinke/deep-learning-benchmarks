1f762fd89ee76de40c7ff707c1ba05617f7119c1
Guolin Ke <i@yumumu.me>
2016-10-27 16:31:51 +0800

    update lstm in tensorflow

--- tensorflow/rnn/lstm/lstm.py
+++ tensorflow/rnn/lstm/lstm.py
@@ -197,20 +197,13 @@ class PTBModel(object):
   def __init__(self, is_training, config):
     self.batch_size = batch_size = config.batch_size
     self.num_steps = num_steps = config.num_steps
-    size = config.hidden_size
+    num_units = config.hidden_size
     vocab_size = config.vocab_size
 
     self._input_data = tf.placeholder(tf.int32, [batch_size, num_steps])
     self._targets = tf.placeholder(tf.int32, [batch_size, num_steps])
 
-    # Slightly better results can be obtained with forget gate biases
-    # initialized to 1 but the hyperparameters of the model would need to be
-    # different than reported in the paper.
-    lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(size, forget_bias=0.0, state_is_tuple=True)
-    if is_training and config.keep_prob < 1:
-      lstm_cell = tf.nn.rnn_cell.DropoutWrapper(
-          lstm_cell, output_keep_prob=config.keep_prob)
-    cell = tf.nn.rnn_cell.MultiRNNCell([lstm_cell] * config.num_layers, state_is_tuple=True)
+
     
     # print config
     print("batch_size: ", config.batch_size)
@@ -218,35 +211,42 @@ class PTBModel(object):
     print("hidden_size: ", config.hidden_size)
     print("num_layers: ", config.num_layers)
 
+    embedding = tf.get_variable("embedding", [vocab_size, num_units])
+    inputs = tf.nn.embedding_lookup(embedding, self._input_data)
+    inputs = [tf.squeeze(input_, [1]) for input_ in tf.split(1, num_steps, inputs)]
+
+    # Slightly better results can be obtained with forget gate biases
+    # initialized to 1 but the hyperparameters of the model would need to be
+    # different than reported in the paper.
+    lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(num_units, forget_bias=0.0, state_is_tuple=True)
+
+    cell = tf.nn.rnn_cell.MultiRNNCell([lstm_cell] * config.num_layers, state_is_tuple=True)
+
     self._initial_state = cell.zero_state(batch_size, tf.float32)
 
-    with tf.device("/cpu:0"):
-      embedding = tf.get_variable("embedding", [vocab_size, size])
-      inputs = tf.nn.embedding_lookup(embedding, self._input_data)
+    outputs, state = tf.nn.rnn(cell, inputs, initial_state=self._initial_state)
 
-    if is_training and config.keep_prob < 1:
-      inputs = tf.nn.dropout(inputs, config.keep_prob)
 
-    # Simplified version of tensorflow.models.rnn.rnn.py's rnn().
-    # This builds an unrolled LSTM for tutorial purposes only.
-    # In general, use the rnn() or state_saving_rnn() from rnn.py.
-    #
-    # The alternative version of the code below is:
-    #
-    # from tensorflow.models.rnn import rnn
-    # inputs = [tf.squeeze(input_, [1])
-    #           for input_ in tf.split(1, num_steps, inputs)]
-    # outputs, state = rnn.rnn(cell, inputs, initial_state=self._initial_state)
-    outputs = []
-    state = self._initial_state
-    with tf.variable_scope("RNN"):
-      for time_step in range(num_steps):
-        if time_step > 0: tf.get_variable_scope().reuse_variables()
-        (cell_output, state) = cell(inputs[:, time_step, :], state)
-        outputs.append(cell_output)
 
-    output = tf.reshape(tf.concat(1, outputs), [-1, size])
-    softmax_w = tf.get_variable("softmax_w", [size, vocab_size])
+    # #use cudnn_rnn
+    # model = tf.contrib.cudnn_rnn.CudnnLSTM(config.num_layers, num_units, num_units)
+    # params_size_t = model.params_size()
+
+    # input_h = tf.Variable(tf.zeros([config.num_layers, batch_size, num_units]))
+    # input_c = tf.Variable(tf.zeros([config.num_layers, batch_size, num_units]))
+
+    # self._initial_state = (input_c, input_h)
+
+    # params = tf.Variable(tf.ones([params_size_t]), validate_shape=False)
+    # outputs, output_h, output_c = model( is_training=True, input_data=inputs, 
+    #   input_h=input_h, input_c=input_c, params=params)
+
+    # state = (output_c, output_h)
+
+
+    output = tf.reshape(tf.concat(1, outputs), [-1, num_units])
+
+    softmax_w = tf.get_variable("softmax_w", [num_units, vocab_size])
     softmax_b = tf.get_variable("softmax_b", [vocab_size])
     logits = tf.matmul(output, softmax_w) + softmax_b
     loss = tf.nn.seq2seq.sequence_loss_by_example(
@@ -322,18 +322,17 @@ def run_epoch(session, m, data, eval_op, ITERS, verbose=False):
   start_time = time.time()
   costs = 0.0
   iters = 0
-  state = session.run(m.initial_state)
-  PrintParameterCount()
+  #state = session.run(m.initial_state)
+  #PrintParameterCount()
   for step, (x, y) in enumerate(ptb_iterator(data, m.batch_size,
                                                     m.num_steps)):
     cost, state, _ = session.run([m.cost, m.final_state, eval_op],
                                  {m.input_data: x,
-                                  m.targets: y,
-                                  m.initial_state: state})
+                                  m.targets: y})
     costs += cost
     iters += m.num_steps
 
-    if verbose and step % (epoch_size // 10) == 10:
+    if verbose:
       print("%.3f perplexity: %.3f speed: %.0f wps" %
             (step * 1.0 / epoch_size, np.exp(costs / iters),
              iters * m.batch_size / (time.time() - start_time)))
